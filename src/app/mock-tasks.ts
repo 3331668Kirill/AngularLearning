5e132accb0aa5fcae3f1b36acc047130c0fbe28a
Kirill <3331668@mail.ru>
2022-11-24 10:33:26 +0300

    tasks items

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import {Task} from "./Task";
+
+export const Tasks:Task[] = [
+  {
+    id:1,
+    text: 'task1',
+    day: '21/04/22',
+    reminder:true
+  },
+  {
+    id:2,
+    text: 'task2',
+    day: '23/04/22',
+    reminder:true
+  },
+  {
+    id:3,
+    text: 'task3',
+    day: '24/04/22',
+    reminder:true
+  },
+]
+
+
